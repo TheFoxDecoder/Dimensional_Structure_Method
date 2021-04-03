84cd9071b2d7c72bce24f14131d7149ee761e694
TheFoxDecoder <tharuka.kalhara98917@gmail.com>
2021-04-04 04:03:29 +0530

    as always call the automated function

--- main.cpp
+++ main.cpp
@@ -1,17 +1,7 @@
-
-#include <iostream>
 #include "dimentional_cal.hpp"
-#include "test.hpp"
-
-
-unsigned long long int op,vert=1;
 
 int main(){
-  for(int i=0;i<65536;++i){
-    op = out_cog(i,i);
-      // testing case the valuse
-    test_ver(i,i,op);
-    //vert = vert*2;
-  }
+  Dcal dcal = Dcal();
+   
   return 0;
 }
