3d25ea98f237c7e1fda397651254090782275ac0
TheFoxDecoder <tharuka.kalhara98917@gmail.com>
2021-04-17 14:06:16 +0530

    created the new dir to code py files with the intererconnections

---
+++ py/surface.py
@@ -0,0 +1,21 @@
+"""
+    This is just a thought/ dream i saw migh work
+    Crossing over multiple number of positions over the case with connections and makingn the connections between them   
+    it sound boring and complicated  but it's really not so just read the code. well you probobly not get it doesnt matter 
+"""
+
+import dask.array as da 
+
+
+class surface:    
+    def __init__(self,num_nodes,dimensions):
+        self.node_numbers= num_nodes # number of nodes to build  
+        self.dimensions = dimensions # number of dimentinos will be building    
+    
+    def building_node(self,type):
+        """
+            to  build the connections will be the heald with the controling inharitance  
+        """
+    
+    
+
