cd4d224a82c452768e25fd65477543d51644f2b7
TheFoxDecoder <tharuka.kalhara98917@gmail.com>
2021-06-02 16:11:01 +0530

    made it a bit accessible

--- py/surface.py
+++ py/surface.py
@@ -1,21 +1,24 @@
 """
-    This is just a thought/ dream i saw migh work
-    Crossing over multiple number of positions over the case with connections and makingn the connections between them   
-    it sound boring and complicated  but it's really not so just read the code. well you probobly not get it doesnt matter 
+    This is just a thought/ dream i saw migh work Crossing over multiple number of positions over the case with
+    connections and making the connections between them it sound boring and complicated  but
+    it's really not so just read the code. well you probably not get it doesnt matter
 """
 
-import dask.array as da 
+from structurePlot import DimensionCalculation as Dcal
 
 
-class surface:    
-    def __init__(self,num_nodes,dimensions):
-        self.node_numbers= num_nodes # number of nodes to build  
-        self.dimensions = dimensions # number of dimentinos will be building    
-    
-    def building_node(self,type):
+# build the cognition surface
+class Surface:
+
+    def __init__(self, num_nodes, dimensions):
+        self.node_numbers = num_nodes  # number of nodes to build
+        self.dimensions = dimensions  # number of dimensions will be building
+
+    def generate_plot(self):
         """
-            to  build the connections will be the heald with the controling inharitance  
+        whole next work get into the graph image and networking
+        :return: plot image
         """
-    
-    
-
+    def calculation(self):
+        Dcal(self.dimensions)
+        Dcal.struct_plot()
